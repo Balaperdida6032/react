a4ea59a66fc80fd69f6d76546b5dc58fa6d9caaa
AgustinBartaburu <bartaburuagustin@gmail.com>
2024-10-24 09:46:08 -0300

    primer commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+//import { StrictMode } from 'react'
+//import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById
+('root')) 
+
+root.render(
+  <App />
 )
+/*createRoot(document.getElementById('root')).render(
+  "Hola mundo"
+)*/
